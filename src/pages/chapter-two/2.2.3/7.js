587f21054ccd79b0521a4abebbe4a3ecdad21cc6
yinhaiying <yinhaiying@58.com>
2020-12-04 10:37:20 +0800

    实现数组的交换

--- src/pages/chapter-two/2.2.3/7.js
+++ src/pages/chapter-two/2.2.3/7.js
@@ -2,7 +2,7 @@
 将两个有序顺序表合并成一个新的有序顺序表，并由函数返回结果顺序表。
 */
 
-const merge = (arr1, arr2) => {
+const merge1 = (arr1, arr2) => {
   let i = 0;
   let j = 0;
   let result = [];
@@ -18,7 +18,30 @@ const merge = (arr1, arr2) => {
   return result;
 }
 
-
+// 更容易理解的写法：
+const merge = (arr1, arr2) => {
+  let i = 0;
+  let j = 0;
+  let result = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i += 1;
+    } else if (arr1[i] >= arr2[j]) {
+      result.push(arr2[j]);
+      j += 1;
+    };
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i += 1;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j += 1;
+  }
+  return result;
+}
 let arr1 = [1, 3, 8, 11, 16, 19, 22];
 let arr2 = [2, 5, 6, 7, 10, 28];
 let arr3 = [1, 1, 1, 1];
