d20244c3750beb5296ce60bcac1fc80ffde1a763
yinhaiying <yinhaiying@58.com>
2020-12-10 15:36:26 +0800

    不使用递归删除所有值为x的结点

--- src/pages/chapter-two/exercises/2.3.7/1.js
+++ src/pages/chapter-two/exercises/2.3.7/1.js
@@ -20,6 +20,30 @@ const removeAllX = (current,x) => {
         removeAllX(current, x);
     }
 }
+// console.log(linkedList.toString());
+// console.log(removeAllX(linkedList.head,5));
+// console.log(linkedList.toString());
+
+// 不使用递归
+const deleteAllX = (x) => {
+    if(linkedList.head === null){
+        return false;
+    }else if(linkedList.head.value === x){
+        linkedList.head = linkedList.head.next;
+    }
+    let prev = linkedList.head;
+    let current = linkedList.head.next;
+    while(current !== null){
+        if(current.value === x){
+            prev.next = current.next;
+            current = current.next;
+        }else{
+            prev = current;
+            current = current.next;
+        }
+    }
+}
+
 console.log(linkedList.toString());
-console.log(removeAllX(linkedList.head,5));
+console.log(deleteAllX( 2));
 console.log(linkedList.toString());
