f0ad8575e67c9d328201535dd79a613a4f4cba11
yinhaiying <18840842571@163.com>
2021-02-12 13:39:17 +0800

    栈的简单实现

---
+++ src/pages/chapter-three/notes/ArrayStack.js
@@ -0,0 +1,46 @@
+
+
+class Stack {
+  constructor() {
+    this.stack = new Array();
+    this.size = 0;
+  }
+  push(element) {
+    this.stack.push(element);
+    this.size += 1;
+  }
+  remove() {
+    if (!this.isEmpty()) {
+      this.size -= 1;
+      return this.stack.pop();
+    }
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
+  peak() {
+    return this.stack[this.size - 1];
+  }
+  toString() {
+    let str = 'Stack:[';
+    for (var i = 0; i < this.size; i++) {
+      str += this.stack[i];
+      if (i !== this.size - 1) {
+        str += ', ';
+      }
+    }
+    str += '] top';
+    return str;
+  }
+}
+
+
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.remove(3);
+stack.push(4);
+stack.push(3);
+console.log(stack.peak());
+console.log(stack.toString())
