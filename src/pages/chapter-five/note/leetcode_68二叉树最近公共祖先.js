2a6f628ffefa911d2d537f2018a453e8082d106c
yinhaiying <18840842571@163.com>
2021-02-13 17:50:00 +0800

    查找公共祖先结点

---
+++ src/pages/chapter-five/note/leetcode_68二叉树最近公共祖先.js
@@ -0,0 +1,49 @@
+/*
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+
+
+输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+输出: 3
+解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+
+分析思路：公共祖先，存在以下几种情况
+1. p和q分别在左右两侧，那么他们的公共祖先一定是根节点root。
+2. p和q同时在左侧，那么只需要查找左侧结点即可。
+3. p和q同时在右侧，那么只需要查找右侧结点即可。
+
+因此，关键的核心就是要判断p和q结点在哪一侧，需要定义一个函数来判断。
+
+*/
+
+
+var lowestCommonAncestor = function (root, p, q) {
+  if (root === null || root === p || root === q) {
+    return root;
+  }
+  var find = function (root, node) {
+    if (root === null || root === node) { return root };
+    var left = find(root.left, node);
+    var right = find(root.right, node);
+    if (left === null) {
+      return right;
+    } else {
+      return left;
+    }
+  }
+  // 先找出p,q分别在哪个位置
+  var pInLeft = find(root.left, p) !== null;
+  var pInRight = !pInLeft;
+  var qInLeft = find(root.left, q) !== null;;
+  var qInRight = !qInLeft;
+  if (pInLeft && qInLeft) {
+    return lowestCommonAncestor(root.left, p, q)
+  } else if (pInRight && qInRight) {
+    return lowestCommonAncestor(root.right, p, q)
+  } else {
+    return root;
+  }
+};
